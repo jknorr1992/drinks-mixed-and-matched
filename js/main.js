a27913519a7b30cce841eaa177c2670b70e19716
Jonathan Knorr <jonathanknorr1992@gmail.com>
2022-04-12 14:29:39 -0700

    Initial cocktailDB integration

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+let recipeInput = document.getElementById("cocktailName");
+let searchButton = document.getElementById("cocktailSearch");
 
+searchButton.addEventListener("click", ()=>{getCocktailByName(recipeInput.value)})
+
+function getCocktailByName(name)
+{
+    console.log(fetch(`www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`));
+}
