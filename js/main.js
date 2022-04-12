70ff0355c7ee8e5bb9e24091f8cbf5b6dd1cfb07
Jonathan Knorr <jonathanknorr1992@gmail.com>
2022-04-12 16:28:07 -0700

    Display search titles

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 let recipeInput = document.getElementById("cocktailName");
 let searchButton = document.getElementById("cocktailSearch");
+let resultsSection = document.getElementById("resultsSection");
+let recipeResults;
 
-searchButton.addEventListener("click", ()=>{getCocktailByName(recipeInput.value)})
+searchButton.addEventListener("click",async ()=> { await getCocktailByName(recipeInput.value);updateSearchResults()})
 
-function getCocktailByName(name)
+async function getCocktailByName(name)
 {
-    console.log(fetch(`www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`));
+    const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`);
+    const data = await response.json();
+    recipeResults = await data;
+}
+
+function updateSearchResults()
+{
+    resultsSection.innerHTML = "";
+    recipeResults.drinks.forEach(element => {
+        const newDiv = resultsSection.appendChild(document.createElement("div"))
+        const text = newDiv.appendChild(document.createElement("p"));
+        text.innerText = element.strDrink;
+    });
 }
